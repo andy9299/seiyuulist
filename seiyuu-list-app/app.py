fad73e87a3c41b98367b27f1d1d8348830818a66
andy9299 <andy.tu998@gmail.com>
2022-12-03 18:12:11 -0500

    added some basic error handling

--- seiyuu-list-app/app.py
+++ seiyuu-list-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash, session, jsonify, g
+from flask import Flask, request, redirect, render_template, flash, session, jsonify, g, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 import os
 import requests
@@ -19,7 +19,7 @@ debug = DebugToolbarExtension(app)
 
 
 def get_info_from_show_data(data):
-    """Given a response from jikanapi at /seasons extract the ids form the first page (25)"""
+    """Given a response from jikanapi about a show, extract the data"""
     genres = []
     for genre in data.get("genres"):
         genres.append(genre.get("name"))
@@ -81,10 +81,16 @@ def get_info_from_person_data(person):
     """
     Given a response from jikanapi that has information about a person grab their info.
     """
+    jp_name = ""
+    if person.get("family_name") is not None:
+        jp_name += (person.get("family_name") + " ")
+    if person.get("given_name") is not None:
+        jp_name += person.get("given_name")
+    jp_name = jp_name.strip()
     return {
         "id": person.get("mal_id"),
         "name": person.get("name"),
-        "jp_name": person.get("family_name") + " " + person.get("given_name"),
+        "jp_name": jp_name,
         "image_url": person.get("images").get("jpg").get("image_url"),
         "about": person.get("about"),
         "birthday": person.get("birthday"),
@@ -92,7 +98,13 @@ def get_info_from_person_data(person):
     }
 
 def get_jikan_request(url, params=None):
-    return requests.get(f"{BASE_URL}{url}", params).json()
+    """Function to handle jikan requests"""
+
+    request = requests.get(f"{BASE_URL}{url}", params).json()
+    if 'error' in request:
+        # This means there isn't the correct data in the api request
+        raise KeyError
+    return request
 
 @app.before_request
 def add_user_to_g():
@@ -109,63 +121,92 @@ def add_user_to_g():
 @app.route("/")
 def root():
     """Homepage."""
-    seasonals_req = get_jikan_request("/seasons/now")
-    anime_info = get_info_from_show_data(random.choice(seasonals_req.get("data")))
-    all_seasonals = []
-    page = 1
-    while True:
-        # to prevent rate limiting
-        time.sleep(WAIT_TIME)
-        for show in seasonals_req.get("data"):
-            all_seasonals.append(
-                {
-                    "id": show.get("mal_id"),
-                    "image_url": show.get("images").get("jpg").get("image_url"),
-                    "title": show.get("title"),
-                }
-            )
-        page += 1
-        seasonals_req = get_jikan_request("/seasons/now", {"page": page})
-        if not seasonals_req.get("pagination").get("has_next_page"):
-            break
+    try:
+        seasonals_req = get_jikan_request("/seasons/now")
+        anime_info = get_info_from_show_data(random.choice(seasonals_req.get("data")))
+        all_seasonals = []
+        page = 1
+        while True:
+            # to prevent rate limiting
+            time.sleep(WAIT_TIME)
+            for show in seasonals_req.get("data"):
+                all_seasonals.append(
+                    {
+                        "id": show.get("mal_id"),
+                        "image_url": show.get("images").get("jpg").get("image_url"),
+                        "title": show.get("title"),
+                    }
+                )
+            page += 1
+            seasonals_req = get_jikan_request("/seasons/now", {"page": page})
+            if not seasonals_req.get("pagination").get("has_next_page"):
+                break
 
-    characters_req = get_jikan_request(f"/anime/{anime_info.get('id')}/characters")
-    characters_info = get_info_from_characters_data(characters_req.get("data"), "main")
+        characters_req = get_jikan_request(f"/anime/{anime_info.get('id')}/characters")
+        characters_info = get_info_from_characters_data(characters_req.get("data"), "main")
 
-    return render_template(
-        "home.html",
-        anime_info=anime_info,
-        characters_info=characters_info,
-        all_seasonals=all_seasonals,
-    )
+        return render_template(
+            "home.html",
+            anime_info=anime_info,
+            characters_info=characters_info,
+            all_seasonals=all_seasonals,
+        )
+    except KeyError:
+        flash("Something went wrong with the api request!")
+        return render_template("home.html")
 
 
 @app.route("/person/<int:person_id>")
 def person_info(person_id):
     """View information about a person"""
 
-    person_req = get_jikan_request(f"/people/{person_id}/full")
-    info = get_info_from_person_data(person_req.get("data"))
-    main_roles = get_info_from_characters_data(person_req.get("data").get("voices"), "main")
-    sup_roles = get_info_from_characters_data(
-        person_req.get("data").get("voices"), "supporting"
-    )
+    try:
+        person_req = get_jikan_request(f"/people/{person_id}/full")
+        info = get_info_from_person_data(person_req.get("data"))
+        main_roles = get_info_from_characters_data(person_req.get("data").get("voices"), "main")
+        sup_roles = get_info_from_characters_data(
+            person_req.get("data").get("voices"), "supporting"
+        )
 
-    return render_template(
-        "person.html", info=info, main_roles=main_roles, sup_roles=sup_roles
-    )
+        return render_template(
+            "person.html", info=info, main_roles=main_roles, sup_roles=sup_roles
+        )
+    except KeyError:
+        flash("Something went wrong with the api request!")
+        return redirect(url_for('root'))
 
 
 @app.route("/anime/<int:anime_id>")
 def anime_info(anime_id):
     """View information about an anime"""
+    try:
+        anime_req = get_jikan_request(f"/anime/{anime_id}/full")
+        info = get_info_from_show_data(anime_req.get("data"))
 
-    anime_req = get_jikan_request(f"/anime/{anime_id}/full")
-    info = get_info_from_show_data(anime_req.get("data"))
+        characters_req = get_jikan_request(f"/anime/{anime_id}/characters")
+        main_characters = get_info_from_characters_data(characters_req.get("data"), "main")
+        sup_characters = get_info_from_characters_data(characters_req.get("data"), "supporting")
+        return render_template(
+            "anime.html", info=info, main_characters=main_characters, sup_characters=sup_characters
+        )
+    except KeyError:
+        flash("Something went wrong with the api request!")
+        return redirect(url_for('root'))
 
-    characters_req = get_jikan_request(f"/anime/{anime_id}/characters")
-    main_characters = get_info_from_characters_data(characters_req.get("data"), "main")
-    sup_characters = get_info_from_characters_data(characters_req.get("data"), "supporting")
-    return render_template(
-        "anime.html", info=info, main_characters=main_characters, sup_characters=sup_characters
-    )
+@app.route("/search/")
+def search():
+    try:
+        search_req = get_jikan_request(f"/{request.args.get('type')}/", {'q': request.args.get('q')})
+        pages = search_req.get('pagination').get('last_visible_page')
+        if request.args.get('type') == 'anime':
+            results = []
+            for anime in search_req.get('data'):
+                results.append(get_info_from_show_data(anime))
+        elif request.args.get('type') == 'people':
+            results = []
+            for person in search_req.get('data'):
+                results.append(get_info_from_person_data(person))
+        return render_template("base.html")
+    except KeyError:
+        flash("Something went wrong with the api request!")
+        return redirect(url_for('root'))
