25341131a831772d6c8e7f94928f750dace06441
andy9299 <andy.tu998@gmail.com>
2022-12-03 21:54:47 -0500

    added view character info and searching

--- seiyuu-list-app/app.py
+++ seiyuu-list-app/app.py
@@ -18,83 +18,119 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "qwerty")
 debug = DebugToolbarExtension(app)
 
 
-def get_info_from_show_data(data):
-    """Given a response from jikanapi about a show, extract the data"""
+def get_info_from_anime_data(anime_data):
+    """Given a response from jikanapi about an anime, extract the data"""
     genres = []
-    for genre in data.get("genres"):
-        genres.append(genre.get("name"))
+    if anime_data.get('genres'):
+        for genre in anime_data.get("genres"):
+            genres.append(genre.get("name"))
     return {
-        "id": data.get("mal_id"),
-        "image_url": data.get("images").get("jpg").get("image_url"),
-        "title": data.get("title"),
-        "synopsis": data.get("synopsis"),
-        "rating": data.get("rating"),
+        "id": anime_data.get("mal_id"),
+        "image_url": anime_data.get("images").get("jpg").get("image_url"),
+        "title": anime_data.get("title"),
+        "synopsis": anime_data.get("synopsis"),
+        "rating": anime_data.get("rating"),
         "genres": genres,
-        "type": data.get("type"),
+        "type": anime_data.get("type"),
     }
 
 
-def get_info_from_characters_data(data, role=None):
+def get_info_by_role(data, type, role=None):
     """
-    Given a response from jikanapi that has a list of characters grab the characters' info
-    role is used to specify the role of the character either with 'main' (Main roles), or 'supporting' (Supporting roles).
+    Given a response from jikanapi that has a list of characters/anime
+    filter the list by role with 'main' (Main roles), 
+    or 'supporting' (Supporting roles).
     Not specifying type will grab all the characters.
     """
-    characters = []
-    for character in data:
-        if role[0] == None or character.get("role").lower() == role.lower():
-            curr_character = character.get("character")
-            character_info = {
-                "character_id": curr_character.get("mal_id"),
-                "character_name": curr_character.get("name"),
-                "character_image_url": curr_character
-                .get("images")
-                .get("jpg")
-                .get("image_url"),
-                "role": character.get("role"),
-            }
-            if character.get("voice_actors") is not None and character.get(
-                "voice_actors"
-            ):
-                voice_actor = character.get("voice_actors")[0]
-                character_info["seiyuu_id"] = (
-                    voice_actor.get("person").get("mal_id")
-                )
-                character_info["seiyuu_name"] = (
-                    voice_actor.get("person").get("name")
-                )
-                character_info["seiyuu_image_url"] = (
-                    voice_actor
-                    .get("person")
-                    .get("images")
-                    .get("jpg")
-                    .get("image_url")
-                )
-            if character.get("anime") is not None:
-                character_info["title"] = character.get("anime").get("title")
-                character_info["anime_id"] = character.get("anime").get("mal_id")
-            characters.append(character_info)
-    return characters
+    info = []
+    if type.lower() == "character":
+        for character in data:
+            if role == None or character.get("role").lower() == role.lower():
+                character_info = get_info_from_character_data(character)
+                info.append(character_info)
 
+    elif type.lower() == "anime":
+        for anime in data:
+            if role == None or anime.get("role").lower() == role.lower():
+                anime_info = get_info_from_anime_data(anime.get('anime'))
+                info.append(anime_info)
+    return info
 
-def get_info_from_person_data(person):
+def get_info_from_character_data(character_data):
     """
-    Given a response from jikanapi that has information about a person grab their info.
+    Given a response from jikanapi with information on one character
+    grab the characters info
+    """
+    # when getting information about characters from the jikanApi depending on 
+    # where it is requested there could be a lot of varying nested/named data
+
+    # requested from /anime/ or /people/
+    if character_data.get("character"):
+        curr_character = character_data.get("character")
+        voice_actor = "voice_actors"
+    # requested from /characters/
+    else:
+        curr_character = character_data
+        voice_actor = "voices"
+
+    character_info = {
+        "character_id": curr_character.get("mal_id"),
+        "character_name": curr_character.get("name"),
+        "character_image_url": curr_character
+        .get("images")
+        .get("jpg")
+        .get("image_url"),
+        "role": character_data.get("role"),
+        'about': character_data.get('about')
+    }
+
+    # some characters have no voice actors
+    if character_data.get(voice_actor) is not None and character_data.get(voice_actor):
+        voice_actor_index = 0
+        for i, dict in enumerate(character_data.get(voice_actor)):
+            if dict['language'] == 'Japanese':
+                voice_actor_index = i
+                break
+        voice_actor = character_data.get(voice_actor)[voice_actor_index]
+        character_info["seiyuu_id"] = (
+            voice_actor.get("person").get("mal_id")
+        )
+        character_info["seiyuu_name"] = (
+            voice_actor.get("person").get("name")
+        )
+        character_info["seiyuu_image_url"] = (
+            voice_actor
+            .get("person")
+            .get("images")
+            .get("jpg")
+            .get("image_url")
+        )
+
+    # check if it is from /characters if it is we need every anime instead of 1 so we dont need this
+    if character_data.get("anime") is not None and not isinstance(character_data.get("anime"), list):
+        character_info["title"] = character_data.get("anime").get("title")
+        character_info["anime_id"] = character_data.get("anime").get("mal_id")
+    return character_info
+
+def get_info_from_person_data(person_data):
+    """
+    Given a response from jikanapi that has information about a person 
+    breakdown their information
     """
     jp_name = ""
-    if person.get("family_name") is not None:
-        jp_name += (person.get("family_name") + " ")
-    if person.get("given_name") is not None:
-        jp_name += person.get("given_name")
+    if person_data.get("family_name") is not None:
+        jp_name += (person_data.get("family_name") + " ")
+    if person_data.get("given_name") is not None:
+        jp_name += person_data.get("given_name")
     jp_name = jp_name.strip()
     return {
-        "id": person.get("mal_id"),
-        "name": person.get("name"),
+        "id": person_data.get("mal_id"),
+        "name": person_data.get("name"),
         "jp_name": jp_name,
-        "image_url": person.get("images").get("jpg").get("image_url"),
-        "about": person.get("about"),
-        "birthday": person.get("birthday"),
-        "website_url": person.get("website_url"),
+        "image_url": person_data.get("images").get("jpg").get("image_url"),
+        "about": person_data.get("about"),
+        "birthday": person_data.get("birthday"),
+        "website_url": person_data.get("website_url"),
     }
 
 def get_jikan_request(url, params=None):
@@ -117,13 +153,12 @@ def add_user_to_g():
     else:
         g.user = None
 
-
 @app.route("/")
 def root():
     """Homepage."""
     try:
         seasonals_req = get_jikan_request("/seasons/now")
-        anime_info = get_info_from_show_data(random.choice(seasonals_req.get("data")))
+        anime_info = get_info_from_anime_data(random.choice(seasonals_req.get("data")))
         all_seasonals = []
         page = 1
         while True:
@@ -143,14 +178,14 @@ def root():
                 break
 
         characters_req = get_jikan_request(f"/anime/{anime_info.get('id')}/characters")
-        characters_info = get_info_from_characters_data(characters_req.get("data"), "main")
-
+        characters_info = get_info_by_role(characters_req.get("data"), "character", "main")
         return render_template(
             "home.html",
             anime_info=anime_info,
             characters_info=characters_info,
             all_seasonals=all_seasonals,
         )
+
     except KeyError:
         flash("Something went wrong with the api request!")
         return render_template("home.html")
@@ -163,14 +198,13 @@ def person_info(person_id):
     try:
         person_req = get_jikan_request(f"/people/{person_id}/full")
         info = get_info_from_person_data(person_req.get("data"))
-        main_roles = get_info_from_characters_data(person_req.get("data").get("voices"), "main")
-        sup_roles = get_info_from_characters_data(
-            person_req.get("data").get("voices"), "supporting"
-        )
+        main_roles = get_info_by_role(person_req.get("data").get("voices"), "character", "main")
+        sup_roles = get_info_by_role(person_req.get("data").get("voices"), "character", "supporting")
 
         return render_template(
             "person.html", info=info, main_roles=main_roles, sup_roles=sup_roles
         )
+
     except KeyError:
         flash("Something went wrong with the api request!")
         return redirect(url_for('root'))
@@ -181,32 +215,59 @@ def anime_info(anime_id):
     """View information about an anime"""
     try:
         anime_req = get_jikan_request(f"/anime/{anime_id}/full")
-        info = get_info_from_show_data(anime_req.get("data"))
+        info = get_info_from_anime_data(anime_req.get("data"))
 
         characters_req = get_jikan_request(f"/anime/{anime_id}/characters")
-        main_characters = get_info_from_characters_data(characters_req.get("data"), "main")
-        sup_characters = get_info_from_characters_data(characters_req.get("data"), "supporting")
+        main_characters = get_info_by_role(characters_req.get("data"), "character", "main")
+        sup_characters = get_info_by_role(characters_req.get("data"), "character", "supporting")
         return render_template(
             "anime.html", info=info, main_characters=main_characters, sup_characters=sup_characters
         )
+
+    except KeyError:
+        flash("Something went wrong with the api request!")
+        return redirect(url_for('root'))
+
+@app.route("/character/<int:character_id>")
+def character_info(character_id):
+    """View information about a character"""
+    try:
+        character_req = get_jikan_request(f"/characters/{character_id}/full")
+        character_info = get_info_from_character_data(character_req.get('data'))
+        main_character_anime = get_info_by_role(character_req.get('data').get('anime'), 'anime', 'main')
+        sup_character_anime = get_info_by_role(character_req.get('data').get('anime'), 'anime', 'supporting')
+
+        return render_template(
+            "character.html", 
+            character_info=character_info, main_character_anime=main_character_anime, sup_character_anime=sup_character_anime
+            )
+
     except KeyError:
         flash("Something went wrong with the api request!")
         return redirect(url_for('root'))
 
 @app.route("/search/")
 def search():
+    """Handle Searching"""
     try:
-        search_req = get_jikan_request(f"/{request.args.get('type')}/", {'q': request.args.get('q')})
+        type = request.args.get('type')
+        query = request.args.get('q')
+        page = request.args.get('p')
+        search_req = get_jikan_request(f"/{type}/", {'q': query, 'page': page, 'sort': 'desc', 'order_by': 'favorites'})
         pages = search_req.get('pagination').get('last_visible_page')
-        if request.args.get('type') == 'anime':
-            results = []
+        results = []
+
+        if type == 'anime':
             for anime in search_req.get('data'):
-                results.append(get_info_from_show_data(anime))
-        elif request.args.get('type') == 'people':
-            results = []
+                results.append(get_info_from_anime_data(anime))
+        elif type == 'people':
             for person in search_req.get('data'):
                 results.append(get_info_from_person_data(person))
-        return render_template("base.html")
+        elif type == 'characters':
+            results = get_info_by_role(search_req.get('data'), 'character')
+
+        return render_template("search.html", results=results, pages=pages)
+
     except KeyError:
         flash("Something went wrong with the api request!")
         return redirect(url_for('root'))
